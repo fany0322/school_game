36f36ba1282d2f8f5ce3a0720b54c35ddf632937
fany0322 <fanystar0322@gmail.com>
2023-02-17 09:08:11 +0900

    final

--- api.js
+++ api.js
@@ -1,4 +1,21 @@
-let schoolId = 1
+function getQueryStringObject() {
+    var a = window.location.search.substr(1).split('&');
+    if (a == "") return {};
+    var b = {};
+    for (var i = 0; i < a.length; ++i) {
+        var p = a[i].split('=', 2);
+        if (p.length == 1)
+            b[p[0]] = "";
+        else
+            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+    }
+    return b;
+}
+let name = document.getElementsByClassName('school_name')[0]
+let qs = getQueryStringObject()
+let schoolId = qs.id
+let schoolName = qs.schoolName
+name.innerText=`${schoolName}`
 async function getScore() {
     let response = await fetch(`https://homicide-hog-default-rtdb.asia-southeast1.firebasedatabase.app/score/${schoolId}.json`)
     let data = await response.json()
